bf93989d9d8e800ee23e363c970f805f7b83b2ad
shinhugh <hughbee135@gmail.com>
2019-12-30 15:03:24 -0800

    Added functions

--- dynamicArray.c
+++ dynamicArray.c
@@ -10,28 +10,53 @@
 
 #define EXPANSION_FACTOR 2
 
+/************************************************************/
 
+unsigned char DYARR_OUT_OF_BOUNDS;
+
+/************************************************************/
 
 void * dyArr_getElement(dynamicArray *array, unsigned int index) {
+
+  DYARR_OUT_OF_BOUNDS = 0;
+
+  // Check for out-of-bounds
   if(index >= array->count) {
+    DYARR_OUT_OF_BOUNDS = 1;
     return 0;
   }
+
+  // Get element
   return array->elements[index];
+
 }
 
+/************************************************************/
 
 unsigned char dyArr_setElement(dynamicArray *array, unsigned int index,
   void *element) {
+
+  DYARR_OUT_OF_BOUNDS = 0;
+
+  // Check for out-of-bounds
   if(index >= array->count) {
+    DYARR_OUT_OF_BOUNDS = 1;
     return 0;
   }
+
+  // Set element
   array->elements[index] = element;
+
 }
 
+/************************************************************/
 
 void dyArr_appendElement(dynamicArray *array, void *element) {
+
   unsigned int i;
   void **elements_trans;
+
+  DYARR_OUT_OF_BOUNDS = 0;
 
   // Resize allocation if necessary (double the size)
   if(array->count >= array->alloc_size) {
@@ -48,23 +73,79 @@ void dyArr_appendElement(dynamicArray *array, void *element) {
   // Append new element
   array->elements[array->count] = element;
   array->count++;
+
 }
 
+/************************************************************/
+
+void dyArr_removeElement(dynamicArray *array, unsigned int index) {
+
+  unsigned int i;
+
+  DYARR_OUT_OF_BOUNDS = 0;
+
+  // Check for out-of-bounds
+  if(index >= array->count) {
+    DYARR_OUT_OF_BOUNDS = 1;
+    return;
+  }
+
+  // Shift down elements
+  for(i = index; i < array->count - 1; i++) {
+    array->elements[i] = array->elements[i + 1];
+  }
+
+  // Decrement count
+  (array->count)--;
+
+}
+
+/************************************************************/
+
+void dyArr_removeAllElements(dynamicArray *array) {
+
+  DYARR_OUT_OF_BOUNDS = 0;
+
+  // Set count to 0
+  array->count = 0;
+
+}
+
+/************************************************************/
 
 unsigned int dyArr_getCount(dynamicArray *array) {
+
+  DYARR_OUT_OF_BOUNDS = 0;
+
+  // Get count
   return array->count;
+
 }
 
+/************************************************************/
 
 void dyArr_initialize(dynamicArray *array) {
+
+  DYARR_OUT_OF_BOUNDS = 0;
+
+  // Start with array capacity 1, empty
   array->elements = malloc(sizeof(void*));
   array->count = 0;
   array->alloc_size = 1;
+
 }
 
+/************************************************************/
 
 void dyArr_deinitialize(dynamicArray *array) {
+
+  DYARR_OUT_OF_BOUNDS = 0;
+
+  // Free memory used by pointer array
   if(array->alloc_size > 0) {
     free(array->elements);
   }
+
 }
+
+/************************************************************/
--- dynamicArray.h
+++ dynamicArray.h
@@ -9,24 +9,37 @@
 #ifndef DYNAMICARRAY_H
 #define DYNAMICARRAY_H
 
+/************************************************************/
+
+// Global flags
+
+extern unsigned char DYARR_OUT_OF_BOUNDS;
+
+/************************************************************/
+
+// Type definitions
+
 typedef struct dynamicArray {
   void **elements;
   unsigned int count;
   unsigned int alloc_size;
 } dynamicArray;
 
+/************************************************************/
+
+// Functions
 
 /*
  * Get an element.
  * Return: Element at specified index
- *         0 on out-of-bounds
+ *         0 on out-of-bounds; set DYARR_OUT_OF_BOUNDS
  */
 void * dyArr_getElement(dynamicArray *array, unsigned int index);
 
 /*
  * Set an element.
  * Return: 1 on success
- *         0 on out-of-bounds
+ *         0 on out-of-bounds; set DYARR_OUT_OF_BOUNDS
  */
 unsigned char dyArr_setElement(dynamicArray *array,
   unsigned int index, void *element);
@@ -35,6 +48,20 @@ unsigned char dyArr_setElement(dynamicArray *array,
  * Append an element at the end.
  */
 void dyArr_appendElement(dynamicArray *array, void *element);
+
+/*
+ * Remove the element at the specified index.
+ * Shift all following elements down by 1 index to fill the hole.
+ * Does not free the memory held by the element itself.
+ * Set DYARR_OUT_OF_BOUNDS on out-of-bounds
+ */
+void dyArr_removeElement(dynamicArray *array, unsigned int index);
+
+/*
+ * Remove all elements.
+ * Does not free the memory held by the elements themselves.
+ */
+void dyArr_removeAllElements(dynamicArray *array);
 
 /*
  * Get the number of elements.
@@ -57,6 +84,7 @@ void dyArr_initialize(dynamicArray *array);
  * Must be called a single time before reference to array is lost to avoid
  * memory leaks.
  * Must be preceded by a dyArr_initialize() call.
+ * Does not free the memory held by the elements themselves.
  */
 void dyArr_deinitialize(dynamicArray *array);
 
